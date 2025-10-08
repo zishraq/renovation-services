59d4ac177250e6252c074a65e90d496381106f05
Amzad <amzad.ah@protonmail.com>
2025-10-09 03:50:50 +0600

    Settings configuration

--- renovation_site/settings.py
+++ renovation_site/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
